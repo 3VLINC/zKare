235cd9885b4d921388def9950deb24fd16da9404
Dan Imbrogno <dan@3vl.ca>
2023-06-24 15:40:24 -0400

    Clean up

--- front/src/app/studies/page.tsx
+++ front/src/app/studies/page.tsx
@@ -1,106 +1,86 @@
 "use client";
 import { useState } from "react";
-import {  useAccount, useContractRead, useContractWrite, useNetwork, usePrepareContractWrite } from "wagmi";
-import { transactions } from "../../../../broadcast/ZKare.s.sol/420/run-latest.json";
-import { abi as ZKareABI } from "../../../../abi/ZKare.json";
-import { errorsABI } from "@/utils/misc";
-import { useQuery, gql } from '@apollo/client';
+import {
+  useAccount
+} from "wagmi";
+import { useQuery, gql } from "@apollo/client";
 import { useEas } from "@/shared/Eas";
-import { SchemaEncoder,  } from "@ethereum-attestation-service/eas-sdk";
+import { SchemaEncoder, ZERO_ADDRESS } from "@ethereum-attestation-service/eas-sdk";
 import { useConfig } from "@/shared/Config";
-
 export default function Test() {
-    const [studyName, setStudyName] = useState<string>("");
-    const { chain } = useNetwork();
-    const { eas, offchain, signer } = useEas();
-    const { address } = useAccount();
-    const { eas: { schemas: { doctor }}} = useConfig();
-    const { data: datum } = useQuery(gql`
-        query MyDoctors {
-            attestations(take: 25, where: { schemaId: { equals: "0x234dee4d3e6a625b4121e2042d6267058755e53a2ecc55555da51a1e6f06cc58"}}) {
-                id
-                attester
-                data
-            }
-            }
-`, {
 
-});
-
-    console.log('datum', datum)
-    const contractCallInputs =
-    studyName && chain
-      ? {
-          address: transactions[0].contractAddress as `0x${string}}`,
-          abi: [...ZKareABI, ...errorsABI],
-          functionName: "createStudy",
-          args: [studyName],
-          chain,
+  const [studyName, setStudyName] = useState<string>("");
+  const { eas } = useEas();
+  const { address } = useAccount();
+  const {
+    eas: {
+      schemas: { study },
+    },
+  } = useConfig();
+  const { data: datum, refetch } = useQuery(
+    gql`
+      query MyStudies($address: String!, $attester: String!) {
+        attestations(take: 25, where: { schemaId: { equals: $address }, attester: { equals: $attester } }) {
+          id
+          attester
+          data
         }
-      : {};
-    const { config: writeConfig, error: wagmiSimulateError } = usePrepareContractWrite(contractCallInputs);
-    
-    const { writeAsync } = useContractWrite(writeConfig);
-
-    const { data, error } = useContractRead({
-        abi: [...ZKareABI],
-        address: transactions[0].contractAddress as `0x${string}}`,
-        functionName: 'studyCounter'
-    });
-
-    // console.log('number of studies', (data as BigInt).toString() , error);
-    const createStudy = async () => {
-        if (address) {
-
-            const schemaEncoder = new SchemaEncoder("string name");
-            const encodedData = schemaEncoder.encodeData([
-            { name: "name", value: "Hal Incandenza", type: "string" }
-            ]);
-            console.log('schema is', doctor);
-            // offchain.signOffchainAttestation(
-            //     {
-            //         schema: doctor,
-            //         recipient: "0x6dC9c87776c3dD7BC362c065f1f74fc9F89E52a4",
-            //         revocable: true,
-            //         data: encodedData,
-            //         expirationTime: 0,
-            //         // Unix timestamp of current time
-            //         time: 1671219636,
-            //         nonce: 0,
-            //         version: 1,
-            //         refUID: '0x0000000000000000000000000000000000000000000000000000000000000000'
-
-            //     }, signer).then(
-            //         console.log
-            //     )
-            console.log('schema id', doctor);
-            const tx = await eas.attest(
-                {
-                    schema: doctor,
-                    data: {
-                        recipient: "0x6dC9c87776c3dD7BC362c065f1f74fc9F89E52a4",
-                        revocable: true,
-                        data: encodedData
-                    },
-                }
-            ).then(
-                (tx) => tx.wait()
-            ).then(
-                (id) => console.log('attestation id', id)
-            ).catch(
-                console.error
-            );
-        }
+      }
+    `,
+    {
+      variables: {
+        address: study,
+        attester: address,
+      },
     }
+  );
 
-    const handleStudyNameChange = (e: any) => {
+  const schemaEncoder = new SchemaEncoder("string study");
 
-        setStudyName(e.target.value);
-        
+  const createStudy = async () => {
+    if (address) {
+      
+      const encodedData = schemaEncoder.encodeData([
+        { name: "study", value: studyName, type: "string" },
+      ]);
+
+      await eas
+        .attest({
+          schema: study,
+          data: {
+            recipient: ZERO_ADDRESS,
+            revocable: true,
+            data: encodedData,
+          },
+        })
+        .then((tx) => tx.wait())
+        .then(() => {
+            setStudyName('');
+            refetch();
+        })
+        .catch(console.error);
     }
+  };
 
-    return <div>
-        <input onChange={handleStudyNameChange} value={studyName} />
-        <button onClick={createStudy}>Create Study</button>
+  const handleStudyNameChange = (e: any) => {
+    setStudyName(e.target.value);
+  };
+
+  const studies = (datum?.attestations || []).map((attestation: any) => {
+      console.log(attestation);
+      
+    return schemaEncoder.decodeData(attestation.data).find(
+        ({ name }) => name === 'study'
+    )?.value.value;
+  });
+
+  return (
+    <div>
+      <input onChange={handleStudyNameChange} value={studyName} />
+      <button onClick={createStudy}>Create Study</button>
+      <ul>
+        {studies.map((study: any) => <p style={{color:'white'}}>{study}</p>)}
+      </ul>
     </div>
+  );
 }
--- front/src/shared/Config.tsx
+++ front/src/shared/Config.tsx
@@ -5,7 +5,8 @@ const ConfigContext = createContext({
     eas: {
         contractAddress: "",
         schemas: {
-            doctor: ""
+            doctor: "",
+            study: ""
         }
     }
 });
@@ -15,7 +16,8 @@ export const Config = ({ children }: PropsWithChildren) => {
         eas: {
             contractAddress: "0x1a5650d0ecbca349dd84bafa85790e3e6955eb84",
             schemas: {
-                doctor: "0x234dee4d3e6a625b4121e2042d6267058755e53a2ecc55555da51a1e6f06cc58"
+                doctor: "0x234dee4d3e6a625b4121e2042d6267058755e53a2ecc55555da51a1e6f06cc58",
+                study: "0x4e56f643c8049d7f66206f1b6c2f1d5f4ad7927d527bbca09866ad90311c4e79"
             }
         }}}>
         {children}
