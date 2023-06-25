5ee3a7e8c0668063e529406b22f2a0de2cee3e36
PolarBearJ <jmstevens512@gmail.com>
2023-06-24 21:44:49 -0400

    Initial doctor view setup

---
+++ front/src/app/components/createPatient.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import NewPatient from "./newPatient"
+import 'bulma/css/bulma.min.css';
+
+const CreatePatient = (patients) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const handleOpenModal = () => {
+      setIsOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsOpen(false);
+    };
+  
+    return (
+      <div>
+        
+        <div className="button is-primary" onClick={handleOpenModal}>
+          Create New Patient
+        </div>
+        <NewPatient isOpen={isOpen} onClose={handleCloseModal} patients={patients}/>
+      </div>
+    );
+  };
+  
+  export default CreatePatient;
+
+  
+  
+  
+  
+  
+  
+  
+  
