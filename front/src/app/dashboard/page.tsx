1a3efa3cda03c70e71cc4e3381acd0cfa8f16e91
devnull03 <56480041+devnull03@users.noreply.github.com>
2023-06-25 01:14:29 -0400

    feat: listing studies

--- front/src/app/dashboard/page.tsx
+++ front/src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 import DoctorDashboard from "../components/dashboards/doctorDashboard";
 import ResearcherDashboard from "../components/dashboards/researcherDashboard";
 import Image from "next/image";
-import { Collapse, Text, Grid } from "@nextui-org/react";
+import { Collapse, Text, Grid, Button } from "@nextui-org/react";
 
 export default function Dashboard() {
 
@@ -14,6 +14,15 @@ export default function Dashboard() {
 	const [studies, setStudies] = useState<{
 		name: string,
 		description: string,
+		options: {
+			gender: string,
+			isSmoker: boolean,
+			isOverweight: boolean,
+			bloodType: string,
+			treatment: string,
+			timestamp: string,
+			heartRate: number,
+		}
 	}[]>([]);
 
 	return (
@@ -27,21 +36,28 @@ export default function Dashboard() {
 				(<ResearcherDashboard></ResearcherDashboard>)
 			} */}
 
-			<div className="flex flex-row gap-20 justify-between h-full pt-12">
+			<div className="flex flex-row gap-20 justify-between w-full h-full pt-12">
 
-				<div className="flex flex-col">
-					<Grid.Container gap={2}>
-						<Grid>
+				<div className="flex flex-col w-full justify-start">
+					<Grid.Container gap={2} className="w-full">
+						<Grid className="w-full">
 							<Text h3>My Studies</Text>
-							<Collapse.Group splitted>
-								<Collapse title="Option A">
-									<Text>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-										enim ad minim veniam, quis nostrud exercitation ullamco laboris
-										nisi ut aliquip ex ea commodo consequat.
-									</Text>
-								</Collapse>
+
+							<Collapse.Group splitted className="w-full">
+								{studies.length ? studies.map((study, index) => {
+									return (
+										<Collapse title={study.name} index={index}>
+											<Text>{study.description}</Text>
+										</Collapse>
+									)
+								}) : (
+									<div className="flex flex-col gap-4">
+										<Text >You don't have any studies yet. Create one using the button below</Text>
+										<a href="/studies" className="w-fit">
+											<Button size={"sm"} css={{ background: "Black" }}>Create study</Button>
+										</a>
+									</div>
+								)}
 							</Collapse.Group>
 						</Grid>
 					</Grid.Container>
@@ -49,6 +65,6 @@ export default function Dashboard() {
 
 				<Image src="/undraw_doctors_p6aq 2.svg" alt="img" width={500} height={500} />
 			</div>
-		</div>
+		</div >
 	)
 }
